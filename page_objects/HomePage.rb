47b4500173fc6947f44db6f3488f077b03f87dcf
omid khodaverdian <omid.khodaverdian@gmail.com>
2015-07-25 18:56:53 -0600

    top menu assertions

--- page_objects/HomePage.rb
+++ page_objects/HomePage.rb
@@ -1,5 +1,12 @@
+require 'Page-object'
+
 class HomePage
 	include PageObject
 
-  div(:my_account, :id => 'myAccountActiveIcon')	
+  div(:logo, class: 'logo')
+  link(:featured, id: 'Featured')
+  link(:originals, id: 'Originals')
+  link(:movies, id: 'Movies')
+  link(:free_episodes, id: 'FreeEpisodes')
+
 end
--- spec/test_cases/QA_1234_login.rb
+++ spec/test_cases/QA_1234_login.rb
@@ -1,11 +1,14 @@
 describe 'QA_1234_Login' do
 	it 'logs in to starzplay' do
+		# TODO move out & change to chrome
 		driver = Selenium::WebDriver.for :firefox
 		driver.get 'http://www.starzplay.com'
 
-		home_page = HomePage.new(driver)
-		# expect(0).to eq(0)
-		driver.close
-		
+		home_page = HomePage.new driver
+		expect(home_page.featured_element).to exist
+		expect(home_page.originals_element).to exist
+		expect(home_page.movies_element).to exist
+		expect(home_page.free_episodes_element).to exist
+		driver.close		
 	end
 end
