307c39e417e473150f29ca61c0064bdbda39baba
omid khodaverdian <omid.khodaverdian@gmail.com>
2015-07-29 20:03:54 -0600

    Scrolling submeu wip

--- page_objects/HomePage.rb
+++ page_objects/HomePage.rb
@@ -4,13 +4,19 @@ class HomePage
 	include PageObject
 
   div :logo, class: 'logo'
+
+  #tabs
   link :featured, id: 'Featured'
   link :originals, id: 'Originals'
   link :movies, id: 'Movies'
   link :free_episodes, id: 'FreeEpisodes'
+  
+  #search bar
   text_field :search, id: 'searchTerm'
+ 
+  #submenu carousel 
   div :submenu, id: 'submenu'
   link :submenu_next, id: 'foo3_next'
   link :submenu_previous, id: 'foo3_prev'
-  # image(:highlighted_submenu, id: 'fade_effect_img')
+  span :highlighted_submenu, id: 'fade_effect_span'
 end													
